35a9aa54e1591ea0efbe733b69613794da452f70
FadiBibawy <eng.fadimichel@gmail.com>
2023-12-18 02:34:12 +0100

    added flex-wrap to the author

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -2,7 +2,7 @@ function Testimonial() {
   return (
     <div className="w-full py-10 bg-gray-200">
       <div className="relative flex flex-col px-5 mx-auto space-y-5 md:w-3/4">
-        <div className="flex flex-col items-center justify-center mx-auto space-y-5 md:w-4/5">
+        <div className="flex flex-col w-100 items-center justify-center mx-auto space-y-5 md:w-4/5">
           <h3 className="text-sm leading-10 text-center text-gray-600">
             Building on the bedrock of excellence,{" "}
             <strong>Good Foundation</strong> stands as a testament to unwavering
@@ -14,10 +14,10 @@ function Testimonial() {
             buildings, but enduring landmarks that echo the spirit of quality
             and dedication.
           </h3>
-          <div className="flex flex-row items-center justify-center space-x-5">
-            <h6 className="font-bold">Michael Zekry</h6>
+          <div className=" flex flex-row flex-wrap w-100 items-center justify-center space-x-5 text-sm">
+            <h6 className="font-bold mb-0">Michael Zekry</h6>
             <span className="text-xl font-extrabold text-blue-600">/</span>
-            <h6>Founder & CEO of Good Foundation LLC.</h6>
+            <p className=" mb-0">Founder & CEO of Good Foundation LLC.</p>
           </div>
         </div>
       </div>
